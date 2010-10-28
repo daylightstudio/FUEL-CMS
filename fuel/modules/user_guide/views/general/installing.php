de31f83bcde679497786c8bab0d2d018b344bc21
David McReynolds <dave@thedaylightstudio.com>
2010-10-28 16:35:49 -0700

    ISSUE#1 updated installation documenation to include note for windows users about a change for the INSTALL_ROOT value in the index.php bootstrap file

--- fuel/modules/user_guide/views/general/installing.php
+++ fuel/modules/user_guide/views/general/installing.php
@@ -2,7 +2,8 @@
 <p>The following are steps to installing FUEL:</p>
 <ol>
 	<li>Download the latest version from <a href="http://www.getfuelcms.com" target="_blank">getfuelcms.com</a></li>
-	<li>Place the <dfn>web</dfn> folder onto your webserver and the <dfn>data_backup</dfn> folder in a folder inaccessible to the web</li>
+	<li>Place the <dfn>web</dfn> folder onto your webserver and make sure your <dfn>data_backup</dfn>, <dfn>install</dfn> and <dfn>crons</dfn> folders are in a folder inaccessible to the web <strong>(the .htaccess file has these blocked by default so you may not need to do anything)</strong>.</li>
+	<li>Browse to the index page. <kbd>NOTE: Windows users will need to change the INSTALL_ROOT variable at the top of the page to server path of the fuel folder.</kbd></li>
 	<li>Install the database by first creating the database in MySQL and then running the fuel_schema.sql file found at the root of the downloaded FUEL folder</li>
 	<li>Configure the <dfn>config/database</dfn> file with the proper database connection settings (like with any other CodeIgniter database application)</li>
 	<li>Make the following folders writable:
