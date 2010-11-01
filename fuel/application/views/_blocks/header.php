fdff18d1ed110a3cc0e2ee7feef8336f84feebf1
David McReynolds <dave@thedaylightstudio.com>
2010-10-31 22:43:19 -0700

    fixed header and footer so that they do not use short tags and removed google analytics code

--- fuel/application/views/_blocks/header.php
+++ fuel/application/views/_blocks/header.php
@@ -2,51 +2,47 @@
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
- 	<title><?=fuel_var('page_title', 'FUEL CMS : A Rapid Development CodeIgniter CMS')?></title>
+ 	<title><?php echo fuel_var('page_title', 'FUEL CMS : A Rapid Development CodeIgniter CMS');?></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta http-equiv="Content-Language" content="en-us" />
 	<meta name="ROBOTS" content="ALL" />
 	<meta http-equiv="imagetoolbar" content="no" />
 	<meta name="MSSmartTagsPreventParsing" content="true" />
 
-	<meta name="keywords" content="<?=fuel_var('meta_keywords')?>" />
-	<meta name="description" content="<?=fuel_var('meta_description')?>" />
+	<meta name="keywords" content="<?php echo fuel_var('meta_keywords'); ?>" />
+	<meta name="description" content="<?php echo fuel_var('meta_description'); ?>" />
 
-	<?=css('main')?>
-	<?=css('ie6', '', array('ie_conditional' => 'lte IE 6'))?>
-	<?=css('ie7', '', array('ie_conditional' => 'lte IE 7'))?>
+	<?php echo css('main'); ?>
+	<?php echo css('ie6', '', array('ie_conditional' => 'lte IE 6')); ?>
+	<?php echo css('ie7', '', array('ie_conditional' => 'lte IE 7'));?>
 	
-	<?=js('jquery')?>
-	<?=js('jquery.inputlabel, jquery.viewer, jquery.shadow, jquery.ifixpng.js, cufon_yui, geometric_400-geometric_800.font, jquery.fancyzoom, main')?>
-	<?=js($js)?>
+	<?php echo js('jquery'); ?>
+	<?php echo js('jquery.inputlabel, jquery.viewer, jquery.shadow, jquery.ifixpng.js, cufon_yui, geometric_400-geometric_800.font, jquery.fancyzoom, main');?>
+	<?php echo js($js); ?>
 	
 	
 	<script type="text/javascript" charset="utf-8">
 	//<![CDATA[
-		var basePath = '<?=site_url()?>';
-		var imgPath = '<?=img_path()?>';
+		var basePath = '<?php echo site_url();?>';
+		var imgPath = '<?php echo img_path(); ?>';
 	//]]>
 	</script>
 </head>
 
-<body class="<?=fuel_var('body_class', 'Body Class')?>">
+<body class="<?php echo fuel_var('body_class', 'Body Class');?>">
 <div id="container">
 	<div id="container_inner">
 		<div id="header">
 			<div id="topnav">
 				<ul>
 					<li><a href="http://www.thedaylightstudio.com">About Daylight</a></li>
-					<?php /* ?><li><a href="<?=site_url('features/screenshots')?>">Screenshots</a></li>
-					<li><a href="<?=site_url('faq')?>">FAQs</a></li>
-					<li><a href="<?=site_url('play_demo')?>">Play Demo</a></li>
-					<?php */?>
-					<li class="last"><?=safe_mailto('info@thedaylightstudio.com', 'Contact');?></li>
+					<li class="last"><?php echo safe_mailto('info@thedaylightstudio.com', 'Contact');?></li>
 				</ul>
 			</div>
 			<a href="http://www.thedaylightstudio.com" id="daylight_logo"></a>
 		
 	
-			<a href="<?=site_url()?>" id="fuel_logo">FUEL CMS</a>
+			<a href="<?php echo site_url()?>" id="fuel_logo">FUEL CMS</a>
 
 			<div id="fuel_intro">
 				<h1 id="fuel_cms">FUEL CMS</h1>
