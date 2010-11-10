e38dedc10e325bd29b55e20c93ae2ff056ffe379
David McReynolds <dave@thedaylightstudio.com>
2010-11-09 18:59:58 -0800

    made changes to top description  text for cronjobs module

--- fuel/modules/cronjobs/views/cronjobs.php
+++ fuel/modules/cronjobs/views/cronjobs.php
@@ -10,8 +10,8 @@
 <div id="main_content" class="noaction">
 
 	<div id="main_content_inner">
-	<p class="instructions">Below is the cron jobs (crontab) file that can be used to execute given tasks on a periodic basis. For example cron jobs are perfect
-	for automatically backing up your database. In fact, we already have an example for you at <?=INSTALL_ROOT.'crons'.DIRECTORY_SEPARATOR.'crontab_default.php'?>. Below is an easy to use interface to manage the schedule of the tasks. 
+	<p class="instructions">Below is the cron jobs (crontab) file that can be used to execute given tasks on a periodic basis. For example, cron jobs are perfect
+	for automatically backing up your database. In fact, we already have an example for you at <?=INSTALL_ROOT.'crons'.DIRECTORY_SEPARATOR.'crontab_default.php'?>.
 	We recommend <a href="http://www.google.com/search?client=safari&rls=en-us&q=cron+job+tutorial&ie=UTF-8&oe=UTF-8" target="_blank"><strong>clicking here to learn more about cron jobs</strong></a> if
 	you are not familiar with them already.</p>
 	
