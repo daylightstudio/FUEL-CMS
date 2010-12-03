43681b77c6a7abc26e5a0da99769f87855b3ec89
David McReynolds <dave@thedaylightstudio.com>
2010-12-03 15:35:10 -0800

    changed ['layout'] value in views/_variables/global.php file to the more brief (and preferred) syntax)

--- fuel/application/views/_variables/global.php
+++ fuel/application/views/_variables/global.php
@@ -5,7 +5,7 @@ $CI =& get_instance();
 
 // generic global page variables used for all pages
 $vars = array();
-$vars['layout'] = array('body' => '_layouts/main');
+$vars['layout'] = 'main';
 $vars['page_title'] = 'FUEL CMS : A Rapid Development CodeIgniter CMS';
 $vars['meta_keywords'] = 'CodeIgniter Content Management System, CodeIgniter CMS, ExpressionEngine Alternative';
 $vars['meta_description'] = 'FUEL CMS is a CodeIgniter-based easy to use Content Management System.';
