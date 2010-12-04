58462fbeaad11bedbb61e56eaea8502347eed532
David McReynolds <dave@thedaylightstudio.com>
2010-12-03 22:54:50 -0800

    fixed issue uploading images for modules

--- fuel/modules/fuel/controllers/module.php
+++ fuel/modules/fuel/controllers/module.php
@@ -1186,7 +1186,7 @@ class Module extends Fuel_base_controller {
 
 						//$config['xss_clean'] = TRUE; // causes problem with image if true... so we use the below method
 						$tmp_file = file_get_contents($file_info['tmp_name']);
-						if ($this->sanitize_images AND $this->input->xss_clean($tmp_file, TRUE) === FALSE)
+						if ($this->sanitize_images AND xss_clean($tmp_file, TRUE) === FALSE)
 						{
 							$errors = TRUE;
 							add_error(lang('upload_invalid_filetype'));
