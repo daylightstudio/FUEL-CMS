180e8899a408703c67fbc111391a7d6ef368fd6a
David McReynolds <dave@thedaylightstudio.com>
2010-12-13 13:58:50 -0800

    fixed user guide under modules/advanced to use FUEL_ROUTE constant instead of FUEL_FOLDER due to recent change

--- fuel/modules/user_guide/views/modules/advanced.php
+++ fuel/modules/user_guide/views/modules/advanced.php
@@ -1,6 +1,6 @@
 <h1>Advanced Modules</h1>
-<p>Think of Adanced Modules as another application folder that can contain it's own
-controllers, helpers, libraries, langauge and view files. They can also contain their
+<p>Think of Advanced Modules as another application folder that can contain it's own
+controllers, helpers, libraries, language and view files. They can also contain their
 own assets folders. FUEL itself is an advanced module as us this <dfn>user_guide</dfn> module.
 It is highly recommended that you take a peek at this module's files and any others to get a sense of how
 to create one (it's not too tough!).
@@ -24,8 +24,8 @@ to create one (it's not too tough!).
 			</li>
 			<li>In the routes file, add the fuel routes you want to use. For example, this <strong>user_guide</strong> module has the following routes:
 				<ul>
-					<li>$route[FUEL_FOLDER.'/tools/user_guide'] = 'user_guide';</li>
-					<li>$route[FUEL_FOLDER.'/tools/user_guide/:any'] = 'user_guide/$1';</li>
+					<li>$route[FUEL_ROUTE.'tools/user_guide'] = 'user_guide';</li>
+					<li>$route[FUEL_ROUTE.'tools/user_guide/:any'] = 'user_guide/$1';</li>
 				</ul>
 			</li>
 		</ul>
@@ -34,7 +34,7 @@ to create one (it's not too tough!).
 	<li>Create your controller files. 
 		<ul>
 			<li><strong>Admin Controllers</strong> - Pages that need to be displayed in the admin interface should inherit from the <dfn>fuel/libraries/Fuel_base_controller.php</dfn> and can use the <dfn>_validate_user()</dfn>, protected controller method.</li>
-			<li><strong>Dashboard Controller</strong> - If you add a controler with the name of <dfn>Dashboard</dfn>, then it can get pulled in to the FUEL admin (if the module is in the fuel $config['dashboards'] configuration)</li>
+			<li><strong>Dashboard Controller</strong> - If you add a controller with the name of <dfn>Dashboard</dfn>, then it can get pulled in to the FUEL admin (if the module is in the fuel $config['dashboards'] configuration)</li>
 		</ul>
 	</li>
 	<li>Create your assets folder and add any specific styles you want to use for that module's admin interface in a <dfn>css/{module_name}.css</dfn> file including your menu item icons.</li>
