ddb6801c4d2540c8582e837954bd26e2afdac68e
David McReynolds <dave@thedaylightstudio.com>
2010-12-20 12:52:48 -0800

    fixed issues with turning off comments for blog posts

--- fuel/modules/blog/views/themes/default/post.php
+++ fuel/modules/blog/views/themes/default/post.php
@@ -1,8 +1,8 @@
 <div class="post">
 	<?=fuel_edit($post->id, 'Edit Post', 'blog/posts')?>
-
+	
 	<?=blog_block('post_unpublished', array('post' => $post))?>
-
+	
 	<h1><?=$post->title?> </h1>
 	<div class="post_author_date">
 		Posted on <span class="post_content_date"><?=$post->get_date_formatted()?></span> by <span class="post_author_name"><?=$post->author_name?></span>
@@ -14,35 +14,34 @@
 	
 </div>
 
-<?php if ($post->allow_comments) : ?>
-
 <a name="comments"></a>
 
-	<?php if ($post->comments_count > 0) : ?>
-		<h3>Comments</h3>
-		<div class="comments">
+<?php if ($post->comments_count > 0) : ?>
+	<h3>Comments</h3>
+	<div class="comments">
 
-			<?php foreach($post->comments as $comment) : ?>
+		<?php foreach($post->comments as $comment) : ?>
 
-				<div class="<?=($comment->is_child()) ? 'comment child' : 'comment'?>">
+			<div class="<?=($comment->is_child()) ? 'comment child' : 'comment'?>">
 
-					<a name="comment<?=$comment->id?>"></a>
-					<div class="comment_content">
-						<?php if ($comment->is_by_post_author()) :?>
-						<?=$comment->post->author->get_avatar_img_tag(array('class' => 'img_left'))?>
-						<?php endif; ?>
-						<?=$comment->content_formatted?>
-					</div>
-
-
-					<div class="comment_meta">
-						<cite><?=$comment->author_and_link?>, <?=$comment->get_date_formatted('h:iA / M d, Y')?></cite>
-					</div>
+				<a name="comment<?=$comment->id?>"></a>
+				<div class="comment_content">
+					<?php if ($comment->is_by_post_author()) :?>
+					<?=$comment->post->author->get_avatar_img_tag(array('class' => 'img_left'))?>
+					<?php endif; ?>
+					<?=$comment->content_formatted?>
 				</div>
-			<?php endforeach; ?>
-		</div>
-	<?php endif; ?>
 
+
+				<div class="comment_meta">
+					<cite><?=$comment->author_and_link?>, <?=$comment->get_date_formatted('h:iA / M d, Y')?></cite>
+				</div>
+			</div>
+		<?php endforeach; ?>
+	</div>
+<?php endif; ?>
+
+<?php if ($post->allow_comments) : ?>
 	<div class="comment_form">
 	<a name="comments_form"></a>
 
@@ -59,4 +58,6 @@
 	<?php endif; ?>
 	</div>
 
+<?php else: ?>
+	<p>Comments have been closed.</p>
 <?php endif; ?>
