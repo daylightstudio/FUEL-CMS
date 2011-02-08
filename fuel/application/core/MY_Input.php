d892075d3a9b8e6e4e97e867f9863054ebe84b89
David McReynolds <dave@thedaylightstudio.com>
2011-02-08 10:24:29 -0800

    workaround for CI 2.0 newlines error on windows (issue #332)

---
+++ fuel/application/core/MY_Input.php
@@ -0,0 +1,95 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * FUEL CMS
+ * http://www.getfuelcms.com
+ *
+ * An open source Content Management System based on the 
+ * Codeigniter framework (http://codeigniter.com)
+ *
+ * @package		FUEL CMS
+ * @author		David McReynolds @ Daylight Studio
+ * @copyright	Copyright (c) 2011, Run for Daylight LLC.
+ * @license		http://www.getfuelcms.com/user_guide/general/license
+ * @link		http://www.getfuelcms.com
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * A CodeIgniter MY Input Class Extension
+ *
+ * This extension will hopefully just be temporary until this bug in the core is resolved:
+ * https://bitbucket.org/ellislab/codeigniter/issue/332/newlines-in-textareas-are-duplicated
+ *
+ * @package		FUEL CMS
+ * @subpackage	Libraries
+ * @category	Libraries
+ * @author		David McReynolds @ Daylight Studio
+ * @link		http://www.getfuelcms.com/user_guide/libraries/my_uri
+ */
+
+class MY_Input extends CI_Input {
+
+	// --------------------------------------------------------------------
+
+	/**
+	* Clean Input Data
+	*
+	* This is a helper function. It escapes data and
+	* standardizes newline characters to \n
+	*
+	* @access	private
+	* @param	string
+	* @return	string
+	*/
+	function _clean_input_data($str)
+	{
+		if (is_array($str))
+		{
+			$new_array = array();
+			foreach ($str as $key => $val)
+			{
+				$new_array[$this->_clean_input_keys($key)] = $this->_clean_input_data($val);
+			}
+			return $new_array;
+		}
+
+		// We strip slashes if magic quotes is on to keep things consistent
+		if (function_exists('get_magic_quotes_gpc') AND get_magic_quotes_gpc())
+		{
+			$str = stripslashes($str);
+		}
+
+		// Clean UTF-8 if supported
+		if (UTF8_ENABLED === TRUE)
+		{
+			$str = $this->uni->clean_string($str);
+		}
+
+		// Should we filter the input data?
+		if ($this->_enable_xss === TRUE)
+		{
+			$str = $this->security->xss_clean($str);
+		}
+
+		// Standardize newlines if needed
+		if ($this->_standardize_newlines == TRUE)
+		{
+			if (strpos($str, "\r") !== FALSE)
+			{
+				//$str = str_replace(array("\r\n", "\r"), PHP_EOL, $str);
+
+				/* CHANGED DUE TO PROBLEM ON WINDOWS MACHINE */
+				$str = str_replace(array("\r\n", "\r"), "\n", $str);
+			}
+		}
+
+		return $str;
+	}
+
+
+}
+// END Input class
+
+/* End of file MY_Input.php */
+/* Location: ./application/core/MY_Input.php */
