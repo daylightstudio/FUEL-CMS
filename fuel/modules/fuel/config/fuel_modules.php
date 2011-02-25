6e370a6ba763d3301fc4eed6a77a9befab6742df
David McReynolds <dave@thedaylightstudio.com>
2011-02-24 20:21:16 -0800

    added delete permission to pages module so that normal users can be granted permission to delete

--- fuel/modules/fuel/config/fuel_modules.php
+++ fuel/modules/fuel/config/fuel_modules.php
@@ -30,7 +30,7 @@ $config['modules']['pages'] = array(
 		'list' => '_layouts/module_list', 
 		'create_edit' => '_layouts/module_create_edit', 
 		'delete' => '_layouts/module_delete'),
-	'permission' => array('edit' => 'pages', 'publish' => 'pages_publish'),
+	'permission' => array('edit' => 'pages', 'publish' => 'pages_publish', 'delete' => 'pages_delete'),
 	// 'permission' => array('edit', 'publish', 'delete'),
 	'instructions' => lang('pages_instructions'),
 	'archivable' => TRUE,
