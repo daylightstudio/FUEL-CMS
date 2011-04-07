0d31cc6488b9d0225c9a0bcb5710397d28f00044
David McReynolds <dave@thedaylightstudio.com>
2011-04-07 14:30:59 -0700

    change ids in FUEL layout to help prevent conflicts with form IDs.

--- fuel/modules/fuel/views/_blocks/fuel_footer.php
+++ fuel/modules/fuel/views/_blocks/fuel_footer.php
@@ -1,9 +1,9 @@
 	
 	</div>
 </div>
-<div id="footer">
-	<div id="boilerplate">
-		<span id="copyright"><?=lang('fuel_copyright', date('Y'))?></span>
+<div id="fuel_footer">
+	<div id="fuel_boilerplate">
+		<span id="fuel_copyright"><?=lang('fuel_copyright', date('Y'))?></span>
 		<?=lang('fuel_developed_by', FUEL_VERSION)?>
 	</div>
 </div>
