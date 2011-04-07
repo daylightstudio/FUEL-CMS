327bead239adde4d98a56277cbff3d35b088466b
David McReynolds <dave@thedaylightstudio.com>
2011-04-07 14:04:01 -0700

    localized delete message on module_delete view

--- fuel/modules/fuel/views/_layouts/module_delete.php
+++ fuel/modules/fuel/views/_layouts/module_delete.php
@@ -1,5 +1,5 @@
 <div id="main_top_panel">
-	<h2 class="ico ico_<?=url_title(str_replace('/', '_', $this->module_uri),'_', TRUE)?>"><a href="<?=fuel_url($this->module_uri)?>"><?=$this->module_name?></a> &gt; Delete</h2>
+	<h2 class="ico ico_<?=url_title(str_replace('/', '_', $this->module_uri),'_', TRUE)?>"><a href="<?=fuel_url($this->module_uri)?>"><?=$this->module_name?></a> &gt; <?=lang('action_delete')?></h2>
 </div>
 <div class="clear"></div>
 
@@ -12,7 +12,7 @@
 <div id="main_content_inner">
 
 
-	<p class="instructions">You are about to delete the item:<br/> <span class="delete"><?=$title?></span></p>
+	<p class="instructions"><?=lang('delete_item_message')?><br/> <span class="delete"><?=$title?></span></p>
 	<?=$this->form->open(array('id' => 'form', 'method' => 'post'))?>
 	<?=$this->form->hidden('id', $id)?>
 
