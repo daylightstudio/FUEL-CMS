f4279a061fc5492c162596de916e79552ba05596
David McReynolds <dave@thedaylightstudio.com>
2011-11-17 20:08:19 -0800

    Fixed issue with blog_posts_model->get_allowed_comments() method not returning proper boolean value

--- fuel/modules/blog/models/blog_posts_model.php
+++ fuel/modules/blog/models/blog_posts_model.php
@@ -336,13 +336,13 @@ class Blog_post_model extends Base_module_record {
 	function get_allow_comments()
 	{
 		$CI =& get_instance();
-		if (is_null($this->props['allow_comments']))
+		if (is_null($this->_fields['allow_comments']))
 		{
 			return is_true_val($this->_CI->fuel_blog->settings('allow_comments'));
 		}
 		else
 		{
-			return is_true_val($this->props['allow_comments']);
+			return is_true_val($this->_fields['allow_comments']);
 		}
 	}
 	
