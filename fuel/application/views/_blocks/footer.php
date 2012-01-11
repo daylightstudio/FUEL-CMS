65ca36d38662ee67cfc7421d9f0362151a9ba77d
Jarrod Mosen <digital.fear@live.com>
2012-01-11 14:54:01 +1300

    Update fuel/application/views/_blocks/footer.php

--- fuel/application/views/_blocks/footer.php
+++ fuel/application/views/_blocks/footer.php
@@ -5,10 +5,13 @@
 		
 		</div>
 		<div id="footer_bot">
-			<div id="copyright">FUEL CMS is a product by <a href="http://www.thedaylightstudio.com" target="_blank">Daylight Studio</a>. <br />Copyright &copy; <?php echo date('Y')?> Daylight Studio,  All Rights Reserved.</div>
+			<div id="copyright">FUEL CMS is a product of <a href="http://www.thedaylightstudio.com" target="_blank">Daylight Studio</a>. <br />Copyright &copy; <?php echo date('Y')?> Daylight Studio,  All Rights Reserved.</div>
 		</div>
 	</div>
 </div>
+
+	<?php echo js('jquery, main'); ?>
+	<?php echo js($js); ?>
 
 </body>
 </html>
