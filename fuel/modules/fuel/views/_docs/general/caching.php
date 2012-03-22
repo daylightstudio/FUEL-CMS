52a73dd8128e87efd904029571f1cbeb2a978a52
David McReynolds <dave@thedaylightstudio.com>
2012-03-21 21:45:48 -0700

    user guide updates

--- fuel/modules/fuel/views/_docs/general/caching.php
+++ fuel/modules/fuel/views/_docs/general/caching.php
@@ -1,10 +1,15 @@
 <h1>Caching</h1>
-<p>CodeIgniter, the  is well-known for it's fast performance as a framework. However, that doesn't always mean that your pages will render.
-Database connections and queries, HTTP requests for javascript and CSS 
+<p>CodeIgniter, is well-known for it's fast performance as a framework. However, there are some instances where it makes sense to cache a page or file to prevent 
+excessive database queries and HTTP requests for javascript and CSS. To help with this, FUEL provides several method to cache resources and thus speed up page speed.</p>
 
-Because FUEL CMS is built on top of the lightweight framework lTo improve site performance, FUEL CMS provides several caching options.</p>
+<p>Programmatically, you can access the FUEL's cache methods through the instantiated <a href="<?=user_guide_url('libraries/fuel_layouts')?>">Fuel_cache</a> object like so:</p>
+<pre class="brush:php">
+$this->fuel->cache->clear_all();
+$this->fuel->cache->save('my_cache_id', $data);
+</pre>
 
 <h2>Page Caching</h2>
+<p></p>
 
 <h2>Block Caching</h2>
 
--- fuel/modules/fuel/views/_docs/general/redirects.php
+++ fuel/modules/fuel/views/_docs/general/redirects.php
@@ -8,13 +8,13 @@ A redirect will only work if there are no pages detected by FUEL. It will check 
 delivering a 404 error.
 </p>
 
+<pre class="brush:php">
+$redirect['news/my_old_news_item'] = 'news/my_brand_new_news_item';
+</pre>
+
 <p>Programmatically, you can access the instantiated <a href="<?=user_guide_url('libraries/fuel_redirects')?>">Fuel_redirects</a> object like so:</p>
 <pre class="brush:php">
 $this->fuel->redirects->execute();
-</pre>
-
-<pre class="brush:php">
-$redirect['news/my_old_news_item'] = 'news/my_brand_new_news_item';
 </pre>
 
 
