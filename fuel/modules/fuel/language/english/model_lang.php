d1b23f21684fe48bece5aec1afef7441b32fe77a
David McReynolds <dave@thedaylightstudio.com>
2012-04-03 00:01:16 -0700

    added required_fields error message to model_lang

--- fuel/modules/fuel/language/english/model_lang.php
+++ fuel/modules/fuel/language/english/model_lang.php
@@ -5,6 +5,7 @@ $lang['error_location_parents_match'] = 'Location and parents can\'t match.';
 $lang['error_in_readonly_mode'] = 'The %1s is in readonly mode and can\'t modify data.';
 
 // standard model errors
+$lang['error_required_fields'] = "Please fill out the required field '%1s'";
 $lang['error_valid_email'] = 'Please enter in a valid email address.';
 $lang['error_valid_phone'] = 'Please enter in a valid phone number.';
 $lang['error_method_does_not_exist'] = "Method %1s does not exist.";
