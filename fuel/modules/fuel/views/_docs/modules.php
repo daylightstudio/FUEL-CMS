ef7ad98da4b06e90e9f6785a7a3c2de5c168f53c
David McReynolds <dave@thedaylightstudio.com>
2012-06-17 15:19:14 -0700

    added modules overview page back

---
+++ fuel/modules/fuel/views/_docs/modules.php
@@ -0,0 +1,14 @@
+<h1>Modules</h1>
+<p>Modules are a way for you to extend the functionality of FUEL. 
+The term <dfn>modules</dfn> is used rather loosely and it is entirely possible to 
+nest modules within another module (sometimes called <dfn>sub-modules</dfn>). We refer to these types of modules as <dfn>advanced modules</dfn>.
+In fact, FUEL is no more then an advanced module itself.</p>
+
+<p>Most of the time however, modules are simply just data models (<dfn>simple modules</dfn>) with a form interface to change values.</p>
+<ul>
+	<li><a href="<?=user_guide_url('modules/simple')?>">Simple Modules</a></li>
+	<li><a href="<?=user_guide_url('modules/tutorial')?>">Creating Simple Modules Tutorial</a></li>
+	<li><a href="<?=user_guide_url('modules/advanced')?>">Advanced Modules</a></li>
+	<li><a href="<?=user_guide_url('modules/forms')?>">Module Forms</a></li>
+	<li><a href="<?=user_guide_url('modules/hooks')?>">Module Hooks</a></li>
+</ul>
