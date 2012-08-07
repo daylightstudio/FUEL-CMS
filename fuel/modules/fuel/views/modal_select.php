a722ad47d0e331f43fe9da67e518aed53f4f35b5
David McReynolds <dave@thedaylightstudio.com>
2012-08-07 10:35:05 -0700

    removed extra space between OK and Cancel buttons in modal select

--- fuel/modules/fuel/views/modal_select.php
+++ fuel/modules/fuel/views/modal_select.php
@@ -4,7 +4,7 @@
 	<div class="buttonbar">
 		<ul>
 			<li class="unattached"><a href="#" class="ico ico_no cancel"><?=lang('btn_cancel')?></a></li>
-			<li class="unattached spacer"><a href="#" class="save ico ico_yes cancel" id="submit"><?=lang('btn_ok')?></a></li>
+			<li class="unattached"><a href="#" class="save ico ico_yes cancel" id="submit"><?=lang('btn_ok')?></a></li>
 		</ul>
 	</div>
 	<div class="clear"></div>
