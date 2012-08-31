5435792b5d8666ac1bbd3b451ab2754bb6de4dfe
David McReynolds <dave@thedaylightstudio.com>
2012-08-30 21:58:20 -0700

    fixed broken links and permission issues with manage page in CMS

--- fuel/modules/fuel/views/manage.php
+++ fuel/modules/fuel/views/manage.php
@@ -3,9 +3,10 @@
 	<div class="boxbuttons">
 		<ul>
 			<?php if ($this->fuel->auth->has_permission('users')) : ?><li><a href="<?=fuel_url('users')?>" class="ico_users"><?=lang('module_users')?></a></li><?php endif; ?> 
-			<?php if ($this->fuel->auth->has_permission('manage/permissions')) : ?><li><a href="<?=fuel_url('permissions')?>" class="ico_permissions"><?=lang('module_permissions')?></a></li><?php endif; ?> 
-			<?php if ($this->fuel->auth->has_permission('manage/cache')) : ?><li><a href="<?=fuel_url('manage/cache')?>" class="ico_manage_cache"><?=lang('module_manage_cache')?></a></li><?php endif; ?> 
-			<?php if ($this->fuel->auth->has_permission('activity')) : ?><li><a href="<?=fuel_url('manage/activity')?>" class="ico_logs"><?=lang('module_manage_activity')?></a></li><?php endif; ?> 
+			<?php if ($this->fuel->auth->has_permission('permissions')) : ?><li><a href="<?=fuel_url('permissions')?>" class="ico_permissions"><?=lang('module_permissions')?></a></li><?php endif; ?> 
+			<?php if ($this->fuel->auth->has_permission('cache')) : ?><li><a href="<?=fuel_url('manage/cache')?>" class="ico_manage_cache"><?=lang('module_manage_cache')?></a></li><?php endif; ?> 
+			<?php if ($this->fuel->auth->has_permission('logs')) : ?><li><a href="<?=fuel_url('logs')?>" class="ico_logs"><?=lang('module_manage_activity')?></a></li><?php endif; ?> 
+			<?php if ($this->fuel->auth->has_permission('settings')) : ?><li><a href="<?=fuel_url('settings')?>" class="ico_settings"><?=lang('module_manage_settings')?></a></li><?php endif; ?> 
 		</ul>
 	</div>
 	
