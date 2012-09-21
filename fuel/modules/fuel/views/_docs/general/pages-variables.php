6e0316597a44a4e54b060ed07a598868bd82aefa
David McReynolds <dave@thedaylightstudio.com>
2012-09-21 07:04:29 -0700

    fix for documentation on rendering a CMS page within a controller

--- fuel/modules/fuel/views/_docs/general/pages-variables.php
+++ fuel/modules/fuel/views/_docs/general/pages-variables.php
@@ -84,12 +84,7 @@ class About extends CI_Controller {
 		$vars = array('page_title' => 'Contact : My Website');
 
 		//... form code goes here
-		
-		// load the fuel_page library class and pass it the view file you want to load
-		$this->load->module_library(FUEL_FOLDER, 'fuel_page', array('location' => 'about/contact'));
-		$this->fuel_page->add_variables($vars);
-		$this->fuel_page->render();
-		
+		$this->fuel->pages->render('about/contact', $vars);
 	}
 }
 </pre>
