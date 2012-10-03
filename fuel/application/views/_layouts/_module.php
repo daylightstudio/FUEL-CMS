717aea0f22ec60f08bfe9f80035d14e59af60ab3
David McReynolds <dave@thedaylightstudio.com>
2012-10-03 00:00:28 -0700

    made some changes to the example _module.php layout file

--- fuel/application/views/_layouts/_module.php
+++ fuel/application/views/_layouts/_module.php
@@ -1,14 +1,21 @@
 <?php 
+/*
+This is an example of a Fuel_module_layout where you have a list view and an item view
+*/
+
 $param = uri_segment($segment);
 
+// check if there is a uri segment
 if ($param)
 {
+	// if the uri segment is numeric, then search by key
 	if (is_numeric($param))
 	{
 		$item = fuel_model($model, array('find' => 'key', 'where' => $param));
 	}
 	else
 	{
+		// otherwise we search by find one
 		if (empty($item_where))
 		{
 			$item_where = array($key_field => $param);
@@ -16,13 +23,15 @@ if ($param)
 		$item = fuel_model($model, array('find' => 'one', 'where' => $item_where));
 	}
 	
+	// if no item is found, then do a redirect or 404 if no redirect exists
 	if (empty($item))
 	{
-		show_404();
+		redirect_404();
 	}
 }
 else
 {
+	// if no uri segment, then we do a query on the model to get a list of data
 	if (empty($list_where))
 	{
 		$list_where = array();
@@ -31,12 +40,12 @@ else
 }
 ?>
 
-<?php if ($item) : ?>
+<?php if (!empty($item)) : ?>
 
-	<?=fuel_block($item_block); ?>
+	<?=fuel_block(array('view' => $item_block, 'data' => $item));  ?>
 
 <?php else: ?>
 	
-	<?=fuel_block($list_block); ?>
+	<?=fuel_block(array('view' => $list_block, 'data' => $data)); ?>
 
 <?php endif; ?>
