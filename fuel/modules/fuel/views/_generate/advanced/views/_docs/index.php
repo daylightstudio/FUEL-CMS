6477080edc461838212e3effce4fb5229484ee72
David McReynolds <dave@thedaylightstudio.com>
2013-02-18 09:55:57 -0800

    added default documentation generation for advanced modules

---
+++ fuel/modules/fuel/views/_generate/advanced/views/_docs/index.php
@@ -0,0 +1,8 @@
+<h1>{module_name} Module Documentation</h1>
+<p>This {module_name} module documentation is for version <?={MODULE_NAME}_VERSION?>.</p>
+
+<?=generate_config_info()?>
+
+<?=generate_toc()?>
+
+
