daa4057892c1543e15ca95681470cb3a80040556
Jarrod Mosen <jarrod.mosen@me.com>
2013-04-12 16:34:54 +1200

    Tidy fuel_header a little

--- fuel/modules/fuel/views/_blocks/fuel_header.php
+++ fuel/modules/fuel/views/_blocks/fuel_header.php
@@ -1,22 +1,22 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<meta charset="utf-8">
  	<title><?=$page_title?></title>
 
 	<?=css('screen.min', 'fuel')?>
 
 	<?php foreach($css as $m => $c) : echo css(array($m => $c))."\n\t"; endforeach; ?>
-	<script type="text/javascript">
+	<script>
 		<?=$this->load->module_view(FUEL_FOLDER, '_blocks/fuel_header_jqx', array(), TRUE)?>
 	</script>
-	<?=js('jquery/jquery, jqx/jqx', 'fuel')?>
+	<?=js('jquery/jquery', 'fuel')?>
+	<?=js('jqx/jqx', 'fuel')?>
 	<?=js($this->fuel->config('fuel_javascript'), 'fuel')?>
 	<?php foreach($js as $m => $j) : echo js(array($m => $j))."\n\t"; endforeach; ?>
 
 	<?php if (!empty($this->js_controller)) : ?> 
-	<script type="text/javascript">
+	<script>
 		<?php if ($this->js_controller != 'fuel.controller.BaseFuelController') : ?>
 		jqx.addPreload('fuel.controller.BaseFuelController');
 		<?php endif; ?>
