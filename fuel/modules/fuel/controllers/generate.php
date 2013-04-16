292395799af4a32792e4a782035e3ce2ac2b53cb
David McReynolds <dave@thedaylightstudio.com>
2013-04-16 10:06:37 -0700

    fixed issue with model record class names beginning with S and the S being removed

--- fuel/modules/fuel/controllers/generate.php
+++ fuel/modules/fuel/controllers/generate.php
@@ -481,7 +481,7 @@ class Generate extends Fuel_base_controller {
 		$vars['table'] = $name;
 		$vars['module_name'] = ucwords(humanize($name));
 		$vars['model_name'] = ucfirst($name);
-		$vars['model_record'] = ucfirst(preg_replace('#ie$#', 'y', ltrim($name, 's')));
+		$vars['model_record'] = ucfirst(preg_replace('#ie$#', 'y', rtrim($name, 's')));
 		$vars['ModuleName'] = ucfirst(camelize($name));
 		$vars['MODULE_NAME'] = strtoupper($name);
 		if ($vars['model_name'] == $vars['model_record'])
