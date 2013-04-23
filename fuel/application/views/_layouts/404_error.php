885ce7e80ab5fd1295b68c3c202da75182faef0e
David McReynolds <dave@thedaylightstudio.com>
2013-04-22 21:53:37 -0700

    fixed error on 404_error.php layout

--- fuel/application/views/_layouts/404_error.php
+++ fuel/application/views/_layouts/404_error.php
@@ -2,7 +2,7 @@
 	
 	<div id="main_inner">
 		<div id="error_404">
-			<h1><?php echo fuel_var('heading')$heading; ?></h1>
+			<h1><?php echo fuel_var('heading'); ?></h1>
 			<?php echo fuel_var('body', ''); ?>
 		</div>
 	</div>
