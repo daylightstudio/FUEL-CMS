ead0b0bfee531113658615817f3d3c91c21c4642
David McReynolds <dave@thedaylightstudio.com>
2013-05-04 07:35:48 -0700

    removed image from asset documentation

--- fuel/modules/fuel/views/_docs/general/assets.php
+++ fuel/modules/fuel/views/_docs/general/assets.php
@@ -2,7 +2,6 @@
 <p>The Assets module allows you to manage the images, CSS, and javascript for your sites. You must set the asset folders you want managed
 to have writable permissions. Because assets are saved on the file system and not in a database, you can use 
 an FTP client to manage the assets as well.</p>
-<img src="<?=img_path('examples/screen_assets.jpg', 'user_guide')?>" class="screen" />
 
 <h2>Uploading Assets</h2>
 <p>To upload assets through the CMS, you can use the <dfn>Upload</dfn> button in the Assets module. The upload page provides the following upload options:</p>
