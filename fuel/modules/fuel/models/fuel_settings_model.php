d26971259ef8a2725c0ab2fee192338209986f2d
David McReynolds <dave@thedaylightstudio.com>
2013-05-04 07:46:57 -0700

    added comments to fuel_settings_model

--- fuel/modules/fuel/models/fuel_settings_model.php
+++ fuel/modules/fuel/models/fuel_settings_model.php
@@ -4,9 +4,17 @@ require_once('base_module_model.php');
 
 class Fuel_settings_model extends Base_module_model
 {
-	public $required = array('key');
-	public $serialized_fields = array('value');
+	public $required = array('key'); // The key setting value is required
+	public $serialized_fields = array('value'); // All values are 
 	
+	// --------------------------------------------------------------------
+	
+	/**
+	 * Constructor.
+	 *
+	 * @access	public
+	 * @return	void
+	 */	
 	function __construct()
 	{
 		parent::__construct('fuel_settings');
