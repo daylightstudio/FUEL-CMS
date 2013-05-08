58fa21af011f144391daa9b047e243c736f88b6e
David McReynolds <dave@thedaylightstudio.com>
2013-05-07 22:09:11 -0700

    added some more documentation regarding multi-language sites

--- fuel/modules/fuel/views/_docs/general/localization.php
+++ fuel/modules/fuel/views/_docs/general/localization.php
@@ -1,13 +1,54 @@
-<h1>Localization</h1>
+<h1>Language/Localization</h1>
 <p>FUEL CMS 1.0 now provides basic multi-language support through the CMS for pages and modules. You can configure the number of 
-supported languages to display for your pages in <a href="<?=user_guide_url('installation/configuration')?>">FUEL config</a>.
-Additionally, if you have a simple module has a field named "language", This field name can be configured by specifying
-the  <a href="<?=user_guide_url('modules/simple')?>">simple module's</a> <dfn>language_col</dfn> parameter.
-</p>
+supported languages in the <a href="<?=user_guide_url('installation/configuration')?>">FUEL config</a>
+by adding to the <dfn>$config['languages']</dfn> parameter. The <a href="<?=user_guide_url('libraries/fuel_language')?>">Fuel_language</a> class
+can then be used to help set and retrieve language information.</p>
 
+
+<h2>Creating a Multi-language Website</h2>
+<p>There are a few different ways to add multi-language support to your website:</p>
+
+<h3>CodIgniter's Language Class</h3>
+<p>CodeIgniter comes with a <a href="http://ellislab.com/codeigniter/user-guide/libraries/language.html" target="_blank">Language class</a> which can is fully available for you to leverage in your site.</p>
+
+<h3>CMS Pages</h3>
+<p>When multiple languages are configured in the <a href="<?=user_guide_url('installation/configuration')?>">FUEL config</a>, you will see an additional drop down when editing or creating a page in the CMS.
+Each page location can have multiple languages assigned to it. Then to display the different language versions of the page, you can pass the <dfn>lang</dfn> query string parameter like so:</p>
+<pre class="brush:php">
+http://localhost/about/history?lang=es
+</pre>
+
+<p>Additionally you can set the language of your site for the current user as follows:</p>
+<pre class="brush:php">
+$this->fuel->language->set_selected('es');
+</pre>
+
+<p>That user is then cookied to use that language going forward.</p>
+
+<h3>View Pages</h3>
+<p>Additionally, if your pages are using views  and there is a language value set by the user using <a href="<?=user_guide_url('libraries/fuel_language')?>">Fuel_language</a>, 
+it will first look for a view file in the <span class="file">views/language/{language}/</span> folder and if it doesn't find it, it will default to just the views folder.</p>
+
+<h3>CMS Modules</h3>
+<p>Blocks, Navigation and your own custom simple modules behave a little differently then the Page module. A new record must be created for each language version.
+These modules have a field name of <dfn>language</dfn> that will appear if more then one language is configured. For Blocks and Navigation, 
+the <a href="<?=user_guide_url('helpers/fuel_helper#func_fuel_block')?>">fuel_blocks</a> and <a href="<?=user_guide_url('helpers/fuel_helper#func_fuel_nav')?>">fuel_nav</a> functions 
+have a "language" parameter you can specify which will pull blocks or menu items specific to that language.
+
+<p class="important">This field name can be configured by specifying the <a href="<?=user_guide_url('modules/simple')?>">simple module's</a> <dfn>language_col</dfn> parameter.</p>
+
+
+<h2>Changing the Language Displayed in the CMS</h2>
 <p>The default installation of FUEL CMS currently only comes with the English langauge. 
 However, localization efforts are underway on the <a href="https://github.com/daylightstudio/FUEL-CMS-Languages" target="_blank">FUEL CMS Languages GitHub repo</a>.
-</p>
+If you are interested in helping out with language support, just let us know or send us a pull request (we love those).</p>
+
+
+<p>To add a language to FUEL, you must <a href="https://github.com/daylightstudio/FUEL-CMS-Languages" target="_blank">download the latest 1.0 language folder (if available).</a> and
+place it's contents in the <span class="file">fuel/modules/fuel/language</span> folder. Then, you can set the <dfn>$config['language']</dfn> in the <span class="file">fuel/application/config.php</span>
+file to match the name of the language folder you downloaded. Alternatively, you may click on your user name in the upper right corner of the CMS, and then change the language associated
+with your urser login.</p>
+
 
 <p>The following are some key areas in which you can utilize FUEL's localization functionality.</p>
 
