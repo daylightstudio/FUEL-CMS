058cdc59f88600e8490d63ecaefccb0150030149
David McReynolds <dave@thedaylightstudio.com>
2013-06-23 20:30:46 -0700

    updates to interface documentation

--- fuel/modules/fuel/views/_docs/introduction/interface.php
+++ fuel/modules/fuel/views/_docs/introduction/interface.php
@@ -10,13 +10,14 @@ Different modules can hook into the dashboard view so it may look slightly diffe
 for you based on the modules you have installed.
 </p>
 <div class="screenshot">
-	<div class="overlay" style="top: 15px; left: 700px;">Last Backup</div>
-	<div class="overlay" style="top: 65px; left: 120px;">Standard FUEL Module</div>
-	<div class="overlay" style="top: 195px; left: 120px;">Blog Module</div>
-	<div class="overlay" style="top: 305px; left: 120px;">Site Specific Modules</div>
-	<div class="overlay" style="top: 405px; left: 120px;">Site Development Modules</div>
-	<div class="overlay" style="top: 575px; left: 120px;">FUEL User Management Modules</div>
-	<div class="overlay" style="top: 190px; left: 260px;">Site Specific Documentation</div>
+	<div class="overlay" style="top: 32px; left: 700px;">Last Backup</div>
+	<div class="overlay" style="top: 130px; left: 120px;">Standard FUEL Modules</div>
+	<div class="overlay" style="top: 265px; left: 120px;">Blog Module</div>
+	<div class="overlay" style="top: 385px; left: 120px;">Site Specific Modules</div>
+	<div class="overlay" style="top: 525px; left: 120px;">Installed Tool Modules</div>
+	<div class="overlay" style="top: 685px; left: 120px;">FUEL Management Modules</div>
+	<div class="overlay" style="top: 130px; left: 600px;">Module Dashboards</div>
+	<div class="overlay" style="top: 750px; left: 450px;">Site Specific Documentation</div>
 	<img src="<?=img_path('screens/screen_dashboard.jpg', FUEL_FOLDER)?>" class="screen" />
 </div>
 
@@ -26,13 +27,12 @@ for you based on the modules you have installed.
 <p>Below is the standard list view of the data for a module.
 </p>
 <div class="screenshot">
-	<div class="overlay" style="top: 5px; left: 240px;">View Options</div>
-	<div class="overlay" style="top: 5px; left: 350px;">Create a New Item</div>
-	<div class="overlay" style="top: 65px; left: 200px;">Sortable Columns</div>
-	<div class="overlay" style="top: 115px; left: 700px;">Easy Publishing By <br />Clicking Publish Status</div>
-	<div class="overlay" style="top: 130px; left: 990px;">Delete Multiple Items</div>
-	<div class="overlay" style="top: 10px; left: 740px;">Search Items</div>
-	<div class="overlay" style="top: 10px; left: 900px;">Limit Search Items</div>
+	<div class="overlay" style="top: 25px; left: 240px;">View Options</div>
+	<div class="overlay" style="top: 25px; left: 500px;">Create a New Item</div>
+	<div class="overlay" style="top: 85px; left: 220px;">Sortable Columns</div>
+	<div class="overlay" style="top: 230px; left: 700px;">Easy Publishing By <br />Clicking Publish Status</div>
+	<div class="overlay" style="top: 230px; left: 1020px;">Delete Multiple Items</div>
+	<div class="overlay" style="top: 45px; left: 710px;">Search Items</div>
 	<img src="<?=img_path('screens/screen_pages_list.jpg', FUEL_FOLDER)?>" class="screen" />
 </div>
 
@@ -40,18 +40,18 @@ for you based on the modules you have installed.
 <h2>Module Edit View</h2>
 <p>Below is an edit view for a page item.</p>
 <div class="screenshot">
-	<div class="overlay" style="top: 5px; left: 440px;">Actions Bar</div>
-	<div class="overlay" style="top: 5px; left: 700px;">Select Other Module Items</div>
-	<div class="overlay" style="top: 45px; left: 800px;">Restore From Previous</div>
-	<div class="overlay" style="top: 245px; left: 700px;">Editing Form</div>
-	<div class="overlay" style="top: 15px; left: 260px;">Breadcrumb</div>
+	<div class="overlay" style="top: 12px; left: 540px;">Actions Bar</div>
+	<div class="overlay" style="top: 45px; left: 951px;">Select Other Module Items</div>
+	<div class="overlay" style="top: 112px; left: 756px;">Restore From Previous</div>
+	<div class="overlay" style="top: 245px; left: 650px;">Editing Form</div>
+	<div class="overlay" style="top: 7px; left: 360px;">Breadcrumb</div>
 	<img src="<?=img_path('screens/screen_page_edit.jpg', FUEL_FOLDER)?>" class="screen" />
 </div>
 
 <h2>Edit User Permissions</h2>
 <p>Below is the edit user view.</p>
 <div class="screenshot">
-	<div class="overlay" style="top: 255px; left: 700px;">User Information</div>
-	<div class="overlay" style="top: 505px; left: 540px;">Permissions</div>
+	<div class="overlay" style="top: 255px; left: 560px;">User Information</div>
+	<div class="overlay" style="top: 505px; left: 560px;">Permissions</div>
 	<img src="<?=img_path('screens/screen_users.jpg', FUEL_FOLDER)?>" class="screen" />
 </div>
