97c53cac11650000a68565a272766d90a8bc571d
David McReynolds <dave@thedaylightstudio.com>
2013-06-25 12:43:35 -0700

    added ability to added custom item class for SimplePie

--- fuel/modules/fuel/helpers/simplepie_helper.php
+++ fuel/modules/fuel/helpers/simplepie_helper.php
@@ -46,6 +46,7 @@ function simplepie($feed, $limit = 5, $params = array())
 		'enable_cache' => TRUE,
 		'cache_location' => $CI->config->item('cache_path'),
 		'enable_order_by_date' => TRUE,
+		'item_class' => NULL,
 		);
 	
 	$p = array();
@@ -66,6 +67,24 @@ function simplepie($feed, $limit = 5, $params = array())
 	$CI->simplepie->enable_order_by_date($p['enable_order_by_date']);
 	$CI->simplepie->enable_cache($p['enable_cache']);
 	$CI->simplepie->set_cache_location($p['cache_location']);
+
+	// load in any custom classes
+	if (!empty($p['item_class']))
+	{
+		if (!class_exists($p['item_class']))
+		{
+			if (!empty($p['item_class_path']))
+			{
+				$class_path = $p['item_class_path'];
+			}
+			else
+			{
+				$class_path = APPPATH.'libraries/'.$p['item_class'].'.php';
+			}
+			require_once($class_path);
+		}
+		$CI->simplepie->set_item_class($p['item_class']);
+	}
 	@$CI->simplepie->init();
 	$CI->simplepie->handle_content_type();
 	$feed_data = $CI->simplepie->get_items(0, $limit);
