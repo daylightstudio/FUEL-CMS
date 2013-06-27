8b9b2b0a60e07b9246e42e0da6f374400668bcae
Piet vZ <piet@thedaylightstudio.com>
2013-06-26 21:08:52 -0700

    Setup responsive behaviour for CMS admin

--- fuel/modules/fuel/views/_blocks/fuel_header.php
+++ fuel/modules/fuel/views/_blocks/fuel_header.php
@@ -5,6 +5,8 @@
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
  	<title><?=$page_title?></title>
 
+ 	<meta name="viewport" content="width=device-width">
+
 	<?=css('jqmodal, jquery.tooltip, jquery.treeview, fuel-theme/jquery-ui-1.8.17.custom, fuel', 'fuel')?>
 
 	<?php foreach($css as $m => $c) : echo css(array($m => $c))."\n\t"; endforeach; ?>
