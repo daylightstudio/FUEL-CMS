be2d7ae97b11c4187afb1ca8263fbe3e0fadad3d
David McReynolds <dave@thedaylightstudio.com>
2013-07-15 08:47:27 -0700

    added SQL for upgrading and migrating permissions to fuel_relationships

--- fuel/install/upgrades/fuel_1.0_schema_changes.sql
+++ fuel/install/upgrades/fuel_1.0_schema_changes.sql
@@ -79,3 +79,11 @@ ALTER TABLE `fuel_blog_posts` DROP `image`;
 ALTER TABLE `fuel_blog_posts` ADD `main_image` VARCHAR(100)  NOT NULL  DEFAULT ''  AFTER `author_id`;
 ALTER TABLE `fuel_blog_posts` ADD `list_image` VARCHAR(100)  NOT NULL  DEFAULT ''  AFTER `main_image`;
 ALTER TABLE `fuel_blog_posts` ADD `thumbnail_image` VARCHAR(100)  NOT NULL  DEFAULT ''  AFTER `list_image`;
+
+
+## Migrate permission relationships to relationships table 
+INSERT INTO `fuel_relationships` (`candidate_table`, `candidate_key`, `foreign_table`, `foreign_key`) 
+(SELECT 'fuel_users', `user_id`, 'fuel_permissions', `permission_id` FROM `fuel_user_to_permissions`);
+
+DROP TABLE `fuel_user_to_permissions`;
+
