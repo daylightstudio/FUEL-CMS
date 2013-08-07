c639ffbbbbff22cc1f0cdcfdc3b78838a3e3e240
David McReynolds <dave@thedaylightstudio.com>
2013-08-06 17:02:10 -0700

    fix for deleting an item from the front end using inline editing and it not refreshing the page

--- fuel/modules/fuel/views/modules/module_close_modal.php
+++ fuel/modules/fuel/views/modules/module_close_modal.php
@@ -1,6 +1,14 @@
 <script type="text/javascript">
 //<![CDATA[
-	top.window.fuel.closeModal();
-	top.window.fuel.setNotification(' &nbsp; ', 'warning');
+	if (top.window.fuel && top.window.fuel.closeModal){
+		top.window.fuel.closeModal();	
+	}
+	if (top.window.fuel && top.window.fuel.setNotification){
+		top.window.fuel.setNotification(' &nbsp; ', 'warning');
+
+	// for inline editing on the front end we just refresh the page
+	} else {
+		top.window.location = top.window.location;
+	}
 //]]>
 </script>
