d9018343e68bbf3d5fabf0f315910986a747499b
David McReynolds <dave@thedaylightstudio.com>
2013-09-19 11:39:41 -0700

    added "staging" environment to index.php bootstrap (as an alternative to "testing" environment)

--- fuel/modules/fuel/views/_docs/index.php
+++ fuel/modules/fuel/views/_docs/index.php
@@ -1,18 +1,9 @@
 <a href="http://www.getfuelcms.com" target="_blank"><img src="<?=img_path('fuel_logo.png', 'user_guide')?>" id="fuel_logo" title="Visit getfuelcms.com" alt="FUEL CMS" /></a>
 
 <h1>Welcome FUEL CMS</h1>
-<p>At it's core, FUEL CMS is a modular based, CodeIgniter development platform for 
-creating web applications. You can create your models, views and controllers 
-like normal and only use the CMS part when and if you need it. It's a hybrid of 
-a framework and a CMS.</p>
+<p>FUEL CMS is a modular based, <a href="http://ellislab.com/codeigniter" target="_blank">CodeIgniter</a> development platform for creating web applications. Developers can create theirs models, views and controllers and only use the CMS part when and if they need it. It's a hybrid of a framework and a CMS.</p>
 
-<p>FUEL CMS evolved out of the need for having a development platform that
-was a Framework first and a CMS second. CodeIgniter fit the bill as a 
-lightweight PHP framework that didn't try and do too much behind the scenes.
-It allowed us to cleanly structure our code and use MVC principles (when needed).
-</p>
+<p>FUEL CMS evolved out of the need for having a development platform that was a framework first and a CMS second. CodeIgniter fit the bill as a lightweight, PHP framework, that didn't try and do too much behind the scenes. It allows developers to cleanly structure their code and use MVC principles when needed.</p>
 
-<p>We developed it so it could be easily integrated into your existing projects 
-allowing you to select where and how you want to use the CMS. The interface
-is highly customizable and easily extensible with modules.</p>
+<p><a href="http://www.thedaylightstudio.com" target="_blank">Daylight Studio</a> developed FUEL CMS so it could be easily integrated into existing CodeIgniter projects allowing developers to select where and how they want to use the CMS. The interface is highly customizable and easily extensible with modules.</p>
 
