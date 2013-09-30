bb68f5bd075368c918402e69f68f448540c06c4f
David McReynolds <dave@thedaylightstudio.com>
2013-09-30 16:51:26 -0700

    added HTTPS FALSE site_url parameter for preview_path URLs and main homepage link in the admin

--- fuel/modules/fuel/views/_blocks/fuel_top.php
+++ fuel/modules/fuel/views/_blocks/fuel_top.php
@@ -1,4 +1,4 @@
 <div id="fuel_top">
 	<div id="nav_toggle">Menu</div>
-	<h1 id="fuel_site_name"><a href="<?=site_url()?>"><?=$this->fuel->config('site_name')?></a></h1>
+	<h1 id="fuel_site_name"><a href="<?=site_url('', FALSE)?>"><?=$this->fuel->config('site_name')?></a></h1>
 </div>
