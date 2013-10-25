6901be7aa8a5b65650180e1df373e2b2badd88ac
David McReynolds <dave@thedaylightstudio.com>
2013-10-24 20:11:23 -0700

    fixed repeating background displayed for fuel/tools page

--- fuel/modules/fuel/views/tools.php
+++ fuel/modules/fuel/views/tools.php
@@ -5,7 +5,7 @@
 	<?php 
 	foreach($nav['tools'] as $key => $val) : ?>
 		<?php if ($this->fuel->auth->has_permission($key) && $val != 'View All...') : ?>
-		<li<?php if ($this->nav_selected == $key) {?> class="active"<?php } ?>><a href="<?=fuel_url($key)?>" class="ico_<?=url_title(str_replace('/', '_', $key),'_', TRUE)?>"><?=$val?></a></li>
+		<li<?php if ($this->nav_selected == $key) {?> class="active"<?php } ?>><a href="<?=fuel_url($key)?>"><i class="ico ico_<?=url_title(str_replace('/', '_', $key),'_', TRUE)?>"></i><?=$val?></a></li>
 		<?php endif; ?>
 	<?php endforeach; ?>
 		</ul>
