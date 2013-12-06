513647557fb2048b1706cfc0eed32411bf060db3
David McReynolds <dave@thedaylightstudio.com>
2013-12-06 12:04:37 -0800

    Updated Dashboard icon... finally. That's been bugging me for years.

--- fuel/modules/fuel/views/_docs/general/credits.php
+++ fuel/modules/fuel/views/_docs/general/credits.php
@@ -9,7 +9,7 @@
 	<li><a href="http://markitup.jaysalvat.com/home/" target="_blank">Jay Salvat</a> - for his great textarea editor MarkItUp!</li>
 	<li><a href="http://philsturgeon.co.uk" target="_blank">Phil Sturgeon</a> - for his MY_Parser implementation with Dwoo and Unzip class (and many other great contributions to the CI community including <a href="http://www.pyrocms.com" target="_blank">PyroCMS</a>)</li>
 	<li><a href="http://dwoo.org" target="_blank">Jordi Boggiano</a> - for his Dwoo template library. We tried reinventing the wheel but quickly realized Jordi had already done it.</li>
-	<li><a href="http://www.famfamfam.com/lab/icons/" target="_blank">famfamfam icon set</a> - for their great icon library</li>
+	<li><a href="http://www.famfamfam.com/lab/icons/" target="_blank">famfamfam icon set</a> and <a href="https://github.com/yusukekamiyamane/fugue-icons" target="_blank">Fugue Icons by Yusuke Kamiyamane</a> - for their great icon libraries</li>
 </ul>
 
 <p>And last but not least, the growing FUEL CMS community that has taken the time to provide valuable feedback, bug fixes, and suggestions.</p>
