bd7ddd9347a10a31c561c1f2c855b77444149941
David McReynolds <dave@thedaylightstudio.com>
2013-12-13 00:42:22 -0800

    fixed issue with active warning using singular version of the modules name

--- fuel/modules/fuel/views/_blocks/notification_extra.php
+++ fuel/modules/fuel/views/_blocks/notification_extra.php
@@ -1,7 +1,7 @@
 <div id="notification_extra" class="notification">
-	<?php if (isset($data['published']) && !is_true_val($data['published'])) {?>
+	<?php if (isset($data['published']) AND !is_true_val($data['published'])) {?>
 			<div class="warning ico ico_warn"><?=lang('warn_not_published')?></div>
-	<?php } else if (isset($data['active']) && !is_true_val($data['active'])) {?>
-		<div class="warning ico ico_warn"><?=lang('warn_not_active', strtolower(substr($this->module_name, 0, -1)))?></div>
+	<?php } else if (isset($data['active']) AND !is_true_val($data['active']) AND isset($this->model)) {?>
+		<div class="warning ico ico_warn"><?=lang('warn_not_active', $this->model->singular_name())?></div>
 	<?php } ?>
 </div>
