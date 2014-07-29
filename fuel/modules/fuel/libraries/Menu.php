dd7af4541f9035dcb6c9edb3d2c6518034656df5
Dominic Baranski <dominic.baranski@gmail.com>
2014-07-29 15:12:26 -0400

    Issue #255 - Resolve _render_array not setting 'active'
    Test and set active status properly.

--- fuel/modules/fuel/libraries/Menu.php
+++ fuel/modules/fuel/libraries/Menu.php
@@ -800,6 +800,11 @@ class Menu {
 				$new_key = (isset($val['nav_key'])) ? $val['nav_key'] : $val['location'];
 				$return[$new_key] = $val;
 				
+				if(in_array($key, $this->_active_items))
+				{
+					$return[$new_key]['active'] = true;
+				}
+				
 				if (!empty($subitems))
 				{
 					$level = $level + 1;
