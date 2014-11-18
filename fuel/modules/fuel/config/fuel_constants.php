41dba74600e8b4abf15d0d7a2e06e48175149808
David McReynolds <dave@thedaylightstudio.com>
2014-11-18 11:10:45 -0800

    Updated FUEL_VERSION constant to 1.2.1

--- fuel/modules/fuel/config/fuel_constants.php
+++ fuel/modules/fuel/config/fuel_constants.php
@@ -1,6 +1,6 @@
 <?php 
 // INSTALL_ROOT is defined in the index.php bootstrap file
-define('FUEL_VERSION', '1.2');
+define('FUEL_VERSION', '1.2.1');
 define('MODULES_FOLDER', '../modules');
 define('FUEL_FOLDER', 'fuel');
 define('MODULES_PATH', APPPATH.MODULES_FOLDER.'/');
