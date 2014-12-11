a0bafceb3ac8381dfcc1cbc0e5d923d9da14eb97
Jonathan Lee <jon@specialbrand.net>
2014-12-11 10:42:17 -0800

    Updated so the view handles the content parsing.

--- fuel/application/views/_admin/_fuel_preview.php
+++ fuel/application/views/_admin/_fuel_preview.php
@@ -1,5 +1,7 @@
-<?php 
-$vars['body'] = '<div class="preview_body">'.markdown(strip_javascript($body)).'</div>';
+<?php
+$preview_body = parse_template_syntax(auto_typography($body));
+// $vars['body'] = '<div class="preview_body">'.markdown(strip_javascript($preview_body)).'</div>';
+$vars['body'] = '<div class="preview_body">'.$preview_body.'</div>';
 $this->load->view('_layouts/main', $vars);
-//echo $body;
+//echo $preview_body;
 ?>
