e34d07a2f485973435651509528bef1c003a3c63
David McReynolds <dave@thedaylightstudio.com>
2015-02-09 22:09:35 -0800

    updated events generated model

--- fuel/modules/fuel/views/_generate/model/events_model.php
+++ fuel/modules/fuel/views/_generate/model/events_model.php
@@ -4,9 +4,24 @@ require_once(FUEL_PATH.'models/base_module_model.php');
 
 class Events_model extends Base_module_model {
 
+	// read more about models in the user guide to get a list of all properties. Below is a subset of the most common:
+	public $record_class = 'Event'; // the name of the record class (if it can't be determined)
+	public $filters = array(); // filters to apply to when searching for items
 	public $required = array('name');
+	public $foreign_keys = array(); // map foreign keys to table models
+	public $linked_fields = array(); // fields that are linked meaning one value helps to determine another. Key is the field, value is a function name to transform it. (e.g. array('slug' => 'title'), or array('slug' => arry('name' => 'strtolower')));
+	public $boolean_fields = array(); // fields that are tinyint and should be treated as boolean
+	public $unique_fields = array(); // fields that are not IDs but are unique. Can also be an array of arrays for compound keys
 	public $parsed_fields = array('description', 'description_formatted', 'excerpt', 'excerpt_formatted');
-	
+	public $serialized_fields = array(); // fields that contain serialized data. This will automatically serialize before saving and unserialize data upon retrieving
+	public $has_many = array(); // keys are model, which can be a key value pair with the key being the module and the value being the model, module (if not specified in model parameter), relationships_model, foreign_key, candidate_key
+	public $belongs_to = array(); // keys are model, which can be a key value pair with the key being the module and the value being the model, module (if not specified in model parameter), relationships_model, foreign_key, candidate_key
+	public $formatters = array(); // an array of helper formatter functions related to a specific field type (e.g. string, datetime, number), or name (e.g. title, content) that can augment field results
+	public $display_unpublished_if_logged_in = FALSE;
+	public $form_fields_class = '';  // a class that can extend Base_model_fields and manipulate the form_fields method
+	protected $friendly_name = ''; // a friendlier name of the group of objects
+	protected $singular_name = ''; // a friendly singular name of the object
+
 	public function __construct()
 	{
 		parent::__construct('events'); // table name
@@ -21,10 +36,7 @@ class Events_model extends Base_module_model {
 	
 	public function on_before_clean($values)
 	{
-		if (empty($value['slug']))
-		{
-			$values['slug'] = url_title($values['name'], 'dash', TRUE);
-		}
+		$values = parent::on_before_clean($values);
 		return $values;
 	}
 	
@@ -48,7 +60,6 @@ class Event_model extends Base_module_record {
 	{
 		return date($format, strtotime($this->start_date));
 	}
-	
 
 	public function get_date_range()
 	{
