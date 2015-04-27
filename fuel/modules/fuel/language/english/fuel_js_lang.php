a0546e51efdd33cd5bfc3d7b30b433b1becb97a9
David McReynolds <dave@thedaylightstudio.com>
2015-04-27 16:58:52 -0700

    Added filters_close and filters_option lang values

--- fuel/modules/fuel/language/english/fuel_js_lang.php
+++ fuel/modules/fuel/language/english/fuel_js_lang.php
@@ -44,6 +44,10 @@ $lang['replace_warning'] = 'Are you sure you want to replace the contents of thi
 
 $lang['edit_multi_select_warning'] = 'You must have only have one option selected to edit.';
 
+$lang['filters_close'] = '&#8673; Close';
+$lang['filters_open'] = '&#8675; Open';
+
+
 /*
 |--------------------------------------------------------------------------
 | MarkItUp
