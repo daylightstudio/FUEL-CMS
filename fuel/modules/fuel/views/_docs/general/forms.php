5874d551c5b013a936bf5e11f4fe1174e3796245
David McReynolds <dave@thedaylightstudio.com>
2015-06-04 09:06:09 -0700

    Added documentation for display_input parameter for file field type

--- fuel/modules/fuel/views/_docs/general/forms.php
+++ fuel/modules/fuel/views/_docs/general/forms.php
@@ -447,6 +447,7 @@ $this->form_builder->register_custom_field($key, $custom_field);
 		<li><strong>multiple</strong>: determines whether to allow multiple files to be uploaded by the same field. The default is FALSE</li>
 		<li><strong>display_preview</strong>: determines whether to to display a preview of the asset</li>
 		<li><strong>replace_values</strong>: an array of key/value pairs that can be used to replace any placeholder values in the upload path</li>
+		<li><strong>display_input</strong>: a boolean value that will display an input field for the name of the file which can be helpful to store the uploaded files name to the database</li>
 	</ul>
 
 	<h4>Image Specific</h4>
