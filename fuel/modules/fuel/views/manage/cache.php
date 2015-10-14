99faf0f2843b67fba67d461df590979b1b4f327e
David McReynolds <dave@bydaylight.com>
2015-10-13 21:38:09 -0700

    Removed uneeded form tags for clearing cache

--- fuel/modules/fuel/views/manage/cache.php
+++ fuel/modules/fuel/views/manage/cache.php
@@ -1,7 +1,6 @@
 <div id="fuel_main_content_inner">
 
 <p class="instructions"><?=lang('cache_instructions')?></p>
-<?=$this->form->open(array('id' => 'form', 'method' => 'post'))?>
 
 <div class="buttonbar">
 	<ul>
@@ -11,4 +10,3 @@
 </div>
 
 <?=$this->form->hidden('action', 'cache')?>
-<?=$this->form->close()?>
