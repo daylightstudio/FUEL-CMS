e454c1d94c63fd608c3f1b17432758973c94af61
David McReynolds <dave@bydaylight.com>
2016-02-04 12:06:39 -0800

    Added label_class property per http://forum.getfuelcms.com/discussion/2458/form-builder-label_class#Item_2

--- fuel/modules/fuel/libraries/Form_builder.php
+++ fuel/modules/fuel/libraries/Form_builder.php
@@ -1695,8 +1695,14 @@ class Form_builder {
 			{
 				$id_name = $params['orig_name'];
 			}
+			
+			$class = '';
+			if (isset($params['label_class']) AND !empty($params['label_class']))
+			{
+				$class = ' class="'.$params['label_class'].'"';
+			}
 			$styles = (isset($params['nowrap']) AND $params['nowrap'] === TRUE) ? ' style="white-space: nowrap;"' : '';
-			$str .= "<label for=\"".Form::create_id($id_name)."\" id=\"label_".Form::create_id($id_name)."\"".$styles.">";
+			$str .= "<label for=\"".Form::create_id($id_name)."\"".$class." id=\"label_".Form::create_id($id_name)."\"".$styles.">";
 		}
 		if ($this->tooltip_labels)
 		{
