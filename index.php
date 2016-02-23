323a639c149b4e59f0d2e209b76699f42d492622
David McReynolds <dave@bydaylight.com>
2016-02-23 09:36:46 -0800

    update for environments configuration on index.php

--- index.php
+++ index.php
@@ -130,32 +130,33 @@ else
  * Different environments will require different levels of error reporting.
  * By default development will show errors but testing and live will hide them.
  */
-switch (ENVIRONMENT)
+if (defined('ENVIRONMENT'))
 {
-	case 'development':
-		error_reporting(-1);
-		ini_set('display_errors', 1);
-	break;
+	switch (ENVIRONMENT)
+	{
+		case 'development': case 'testing':
+			error_reporting(-1);
+			ini_set('display_errors', 1);
+		break;
 
-	case 'testing':
-	case 'production':
-		ini_set('display_errors', 0);
-		if (version_compare(PHP_VERSION, '5.3', '>='))
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-		}
-		else
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-		}
-	break;
+		case 'staging': case 'production':
+			ini_set('display_errors', 0);
+			if (version_compare(PHP_VERSION, '5.3', '>='))
+			{
+				error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+			}
+			else
+			{
+				error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+			}
+		break;
 
-	default:
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'The application environment is not set correctly.';
-		exit(1); // EXIT_ERROR
+		default:
+			header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+			echo 'The application environment is not set correctly.';
+			exit(1); // EXIT_ERROR
+	}
 }
-
 /*
  *---------------------------------------------------------------
  * SYSTEM FOLDER NAME
