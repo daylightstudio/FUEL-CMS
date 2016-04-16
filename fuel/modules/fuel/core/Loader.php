3ca90ae6c87fc209dc5e43ceea834b299d8cb75e
David McReynolds <dave@bydaylight.com>
2016-04-15 17:28:30 -0700

    Fix for loading order of paths to use the application folder first

--- fuel/modules/fuel/core/Loader.php
+++ fuel/modules/fuel/core/Loader.php
@@ -419,6 +419,9 @@ class Fuel_Loader extends CI_Loader
 			/* add file extension if not provided */
 			$_ci_file = (pathinfo($_ci_view, PATHINFO_EXTENSION)) ? $_ci_view : $_ci_view.EXT;
 			
+			// <!-- FUEL ... so it will look in the "application" folder first
+			krsort($this->_ci_view_paths);
+
 			foreach ($this->_ci_view_paths as $path => $cascade) 
 			{				
 				if (file_exists($view = $path.$_ci_file)) 
