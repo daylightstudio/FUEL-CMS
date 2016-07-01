995d85ff70743039b063714c711ab4c49d9cb19e
David McReynolds <dave@bydaylight.com>
2016-06-30 22:06:21 -0700

    Removed testing code from default MY_fuel_layouts.php

--- fuel/application/config/MY_fuel_layouts.php
+++ fuel/application/config/MY_fuel_layouts.php
@@ -29,36 +29,9 @@ $config['layouts']['main'] = array(
 		'heading' => array('label' => lang('layout_field_heading')),
 		'body' => array('label' => lang('layout_field_body'), 'type' => 'textarea', 'description' => lang('layout_field_body_description')),
 		'body_class' => array('label' => lang('layout_field_body_class')),
-		'asset' => array('type' => 'asset'),
-		'sections' => array(
-  'type'          => 'template',
-  'display_label' => FALSE,
-  'label'         => 'Sections',
-  'add_extra'     => FALSE,
-  'repeatable'    => TRUE,
-  'title_field'   => 'block',
-  'fields'        => 
-    array(
-      'section'   => array('type' => 'section', 'value' => 'Block <span class="num">{num}</span>'),
-      'block'     => array('type' => 'block', 'label' => 'Block', 'group' => 'landing'),
-    ),
-
-  ),
 	)
 );
 
-$config['blocks']['block'] = array(
-	//'file' 		=> $config['layouts_path'].'main',
-	// 'class'		=> 'Main_layout',
-	// 'filepath' => 'libraries',
-	// 'filename' => 'Main_layout.php',
-	'group' => 'landing',
-	
-	'fields'	=> array(
-		'body' => array('label' => lang('layout_field_body'), 'type' => 'textarea', 'description' => lang('layout_field_body_description')),
-    ),
-
-);
 
 /* End of file MY_fuel_layouts.php */
 /* Location: ./application/config/MY_fuel_layouts.php */
