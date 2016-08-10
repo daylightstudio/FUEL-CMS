1e7776afc1ba8c15bba0a0fa6f63877f6d376a18
David McReynolds <dave@bydaylight.com>
2016-08-10 10:19:37 -0700

    Fixed issue with Loading files from different modules and the same name

--- fuel/modules/fuel/core/Loader.php
+++ fuel/modules/fuel/core/Loader.php
@@ -314,13 +314,97 @@ class Fuel_Loader extends CI_Loader
 
 		list($path, $_view) = Modules::find($view, $module, 'views/');
 		
-		if ($path != FALSE) 
-		{
-			$this->_ci_view_paths = array($path => TRUE) + $this->_ci_view_paths;
-			$view = $_view;
-		}
+		// <!-- FUEL Causes issues if you have 2 view files with the same name in different modules... it may load the other one
+		// if ($path != FALSE) 
+		// {
+		// 	$this->_ci_view_paths = array($path => TRUE) + $this->_ci_view_paths;
+		// 	$view = $_view;
+		// }
 		
+		$this->_ci_view_path = array($path => TRUE);
 		return $this->_ci_load(array('_ci_view' => $view, '_ci_vars' => $this->_ci_object_to_array($vars), '_ci_return' => $return), $scope);
+	}
+
+	public function _ci_load($_ci_data, $scope = NULL) 
+	{
+		extract($_ci_data);
+		
+		if (isset($_ci_view)) 
+		{
+			$_ci_path = '';
+			
+			/* add file extension if not provided */
+			$_ci_file = (pathinfo($_ci_view, PATHINFO_EXTENSION)) ? $_ci_view : $_ci_view.EXT;
+
+			foreach ($this->_ci_view_paths as $path => $cascade) 
+			{				
+				if (file_exists($view = $path.$_ci_file)) 
+				{
+					$_ci_path = $view;
+					break;
+				}
+				if ( ! $cascade) break;
+			}	
+		} 
+		elseif (isset($_ci_path)) 
+		{
+			
+			$_ci_file = basename($_ci_path);
+			if( ! file_exists($_ci_path)) $_ci_path = '';
+		}
+
+		if (empty($_ci_path)) 
+			show_error('Unable to load the requested file: '.$_ci_file);
+
+		$_ci_CI =& get_instance();
+		foreach (get_object_vars($_ci_CI) as $_ci_key => $_ci_var)
+		{
+			if ( ! isset($this->$_ci_key))
+			{
+				$this->$_ci_key =& $_ci_CI->$_ci_key;
+			}
+		}
+
+		if (empty($scope))
+		{
+			$scope = $this->_ci_cached_vars_scope;
+		}
+		if (!isset($this->_ci_cached_vars[$scope]))
+		{
+			$this->_ci_cached_vars[$scope] = array();
+		}
+		if (is_array($_ci_vars))
+		{
+			$this->_ci_cached_vars[$scope] = array_merge($this->_ci_cached_vars[$scope], (array) $_ci_vars);
+		}
+		$vars = $this->_ci_cached_vars[$scope];
+
+		// if the scope is set to TRUE then it is just unique to this view and is not loaded into the global view namespace so we just use that instead
+		extract(($scope === TRUE) ? $_ci_vars : $this->_ci_cached_vars[$scope]);
+
+		ob_start();
+
+		if ((bool) @ini_get('short_open_tag') === FALSE && CI::$APP->config->item('rewrite_short_tags') == TRUE) 
+		{
+			echo eval('?>'.preg_replace("/;*\s*\?>/", "; ?>", str_replace('<?=', '<?php echo ', file_get_contents($_ci_path))));
+		} 
+		else 
+		{
+			include($_ci_path); 
+		}
+
+		log_message('debug', 'File loaded: '.$_ci_path);
+
+		if ($_ci_return == TRUE) return ob_get_clean();
+
+		if (ob_get_level() > $this->_ci_ob_level + 1) 
+		{
+			ob_end_flush();
+		} 
+		else 
+		{
+			CI::$APP->output->append_output(ob_get_clean());
+		}
 	}
 
 	protected function &_ci_get_component($component) 
@@ -407,89 +491,6 @@ class Fuel_Loader extends CI_Loader
 		//$this->_ci_assign_to_models();
 		return CI::$APP->db;
 	}
-
-	public function _ci_load($_ci_data, $scope = NULL) 
-	{
-		extract($_ci_data);
-		
-		if (isset($_ci_view)) 
-		{
-			$_ci_path = '';
-			
-			/* add file extension if not provided */
-			$_ci_file = (pathinfo($_ci_view, PATHINFO_EXTENSION)) ? $_ci_view : $_ci_view.EXT;
-			
-			foreach ($this->_ci_view_paths as $path => $cascade) 
-			{				
-				if (file_exists($view = $path.$_ci_file)) 
-				{
-					$_ci_path = $view;
-					break;
-				}
-				if ( ! $cascade) break;
-			}	
-		} 
-		elseif (isset($_ci_path)) 
-		{
-			
-			$_ci_file = basename($_ci_path);
-			if( ! file_exists($_ci_path)) $_ci_path = '';
-		}
-
-		if (empty($_ci_path)) 
-			show_error('Unable to load the requested file: '.$_ci_file);
-
-		$_ci_CI =& get_instance();
-		foreach (get_object_vars($_ci_CI) as $_ci_key => $_ci_var)
-		{
-			if ( ! isset($this->$_ci_key))
-			{
-				$this->$_ci_key =& $_ci_CI->$_ci_key;
-			}
-		}
-
-		if (empty($scope))
-		{
-			$scope = $this->_ci_cached_vars_scope;
-		}
-		if (!isset($this->_ci_cached_vars[$scope]))
-		{
-			$this->_ci_cached_vars[$scope] = array();
-		}
-		if (is_array($_ci_vars))
-		{
-			$this->_ci_cached_vars[$scope] = array_merge($this->_ci_cached_vars[$scope], (array) $_ci_vars);
-		}
-		$vars = $this->_ci_cached_vars[$scope];
-
-		// if the scope is set to TRUE then it is just unique to this view and is not loaded into the global view namespace so we just use that instead
-		extract(($scope === TRUE) ? $_ci_vars : $this->_ci_cached_vars[$scope]);
-
-		ob_start();
-
-		if ((bool) @ini_get('short_open_tag') === FALSE && CI::$APP->config->item('rewrite_short_tags') == TRUE) 
-		{
-			echo eval('?>'.preg_replace("/;*\s*\?>/", "; ?>", str_replace('<?=', '<?php echo ', file_get_contents($_ci_path))));
-		} 
-		else 
-		{
-			include($_ci_path); 
-		}
-
-		log_message('debug', 'File loaded: '.$_ci_path);
-
-		if ($_ci_return == TRUE) return ob_get_clean();
-
-		if (ob_get_level() > $this->_ci_ob_level + 1) 
-		{
-			ob_end_flush();
-		} 
-		else 
-		{
-			CI::$APP->output->append_output(ob_get_clean());
-		}
-	}
-
 
 	// --------------------------------------------------------------------
 
