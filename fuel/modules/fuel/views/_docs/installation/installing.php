8412f8d0a51d70287889095b5a54c88022aad924
David McReynolds <dave@bydaylight.com>
2017-04-02 21:32:44 -0700

    Updated directions for upgrading

--- fuel/modules/fuel/views/_docs/installation/installing.php
+++ fuel/modules/fuel/views/_docs/installation/installing.php
@@ -30,7 +30,7 @@
 <p>That's it!</p>
 
 <h2>Upgrading to 1.4</h2>
-If you have a current installation and are wanting to upgrade, there are a few things to be aware of. FUEL 1.4 uses CodeIgniter 3.x which includes a number of changes, the most prominent being the capitalization of controller and model names. Additionally it is more strict on reporting errors. FUEL 1.4 includes a script to help automate most (and maybe all) of the updates that may be required in your own fuel/application and installed advanced module code. It is recommended you run the following command using a different branch to test:
+If you have a current installation and are wanting to upgrade, there are a few things to be aware of. FUEL 1.4 uses CodeIgniter 3.x which includes a number of changes, the most prominent being the capitalization of controller and model names. Additionally it is more strict on reporting errors. FUEL 1.4 includes a script to help automate most (and maybe all) of the updates that may be required in your own fuel/application and installed advanced module code. It is recommended you run the following command using a different branch to test if you are running on Mac OSX or a Unix flavor operating system and using Git:
 <pre class="brush: php">
 php index.php fuel/installer/update
 </pre>
