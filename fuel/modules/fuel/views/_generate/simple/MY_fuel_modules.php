466d05a0c1c700208e44657e542987998b8a48c4
Gumster <matthew@heavymeta.co.uk>
2019-01-04 10:42:00 +0000

    Add config item for module_uri
    Without a module_uri definition, action button urls (eg create) won't have the correct path

--- fuel/modules/fuel/views/_generate/simple/MY_fuel_modules.php
+++ fuel/modules/fuel/views/_generate/simple/MY_fuel_modules.php
@@ -1,4 +1,5 @@
 $config['modules']['{module}'] = array(
 	'preview_path' => '', // put in the preview path on the site e.g products/{slug}
 	'model_location' => '{advanced_module}', // put in the advanced module name here
+	'module_uri' => '{advanced_module}/{module}' // IMPORTANT! need to define so actions buttons (eg create) have right urls
 );
