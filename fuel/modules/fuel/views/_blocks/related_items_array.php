24edfbc3cfe02b4ad873ec8a790926af98b2d2f9
David McReynolds <dave@thedaylightstudio.com>
2019-02-08 15:51:25 -0800

    Fixes for if a module doesn’t exist when rendering related items

--- fuel/modules/fuel/views/_blocks/related_items_array.php
+++ fuel/modules/fuel/views/_blocks/related_items_array.php
@@ -7,35 +7,30 @@
 	
 	$mod = $this->fuel->modules->get($group);
 	
-	if ($this->fuel->modules->is_advanced($mod))
-	{
-		$icon = $mod->icon();
-		$name = $mod->friendly_name();
-	}
-	else
-	{
-		$icon = $mod->info('icon_class');
-		$name = $mod->info('module_name');
-	}
-	
-	if (!empty($group_related)) :
+	if ($mod) :
+		if ($this->fuel->modules->is_advanced($mod)) :
+			$icon = $mod->icon();
+			$name = $mod->friendly_name();
+		else:
+			$icon = $mod->info('icon_class');
+			$name = $mod->info('module_name');
+		endif;
+		
+		if (!empty($group_related)) :
 	?>
 
 	<h3 class="ico <?=$icon?>"><?=$name?></h3>
 	<ul>
 		<?php foreach($group_related as $url => $label) : ?>
 		<?php 
-		if ($this->fuel->modules->is_advanced($mod))
-		{
+		if ($this->fuel->modules->is_advanced($mod)) :
 			$url = $mod->fuel_url($url);
-		}
-		else
-		{
+		else :
 			$url = fuel_url($mod->info('module_uri').'/'.$url);
-		}
+		endif;
 		?>
 		<li><a href="<?=$url?>"><?=$label?></a></li>
 		<?php endforeach; ?>
 	</ul>
-	<?php endif; ?>
+	<?php endif;	endif; ?>
 <?php endforeach; ?>
