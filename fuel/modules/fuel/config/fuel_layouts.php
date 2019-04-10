1a23450c760db21306769352348da94679850b6f
David McReynolds <dave@thedaylightstudio.com>
2019-04-10 13:47:55 -0700

    Added ability to rename default templates and they won’t appear as defaults in the Pages layout dropdown

--- fuel/modules/fuel/config/fuel_layouts.php
+++ fuel/modules/fuel/config/fuel_layouts.php
@@ -63,89 +63,108 @@ $config['hidden'] = array();
 
 @include(APPPATH.'config/MY_fuel_layouts.php');
 
-// initialize layout fields 
-$config['layouts']['301_redirect'] = array(
-	'label' => '301 Redirect',
-	'fields' => array(
-		'copy' => array(
-			'type' => 'copy',
-			'label' => lang('layout_field_301_redirect_copy')
-		),
-		'redirect_to' => array('label' => lang('layout_field_redirect_to'))
-	)
-);
-
-$config['layouts']['alias'] = array(
-	'label' => 'Alias',
-	'fields' => array(
-		'copy' => array(
-			'type' => 'copy',
-			'label' => lang('layout_field_alias_copy')
-		),
-		'alias' => array('label' => lang('layout_field_alias')),
-	)
-);
-
-$config['layouts']['404_error'] = array(
-	'label' => '404 Error',
-	'fields' => array(
-		'heading' => array('label' => lang('layout_field_heading')),
-		'body' => array('label' => lang('layout_field_body')),
-	)
-);
-
-$config['layouts']['sitemap_xml'] = array(
-	'label' => 'sitemap.xml',
-	'fields' => array(
-		'copy' => array(
-			'type' => 'copy',
-			'label' => lang('layout_field_sitemap_xml_copy')
-		),
-		'frequency' => array(
-			'type' => 'select',
-			'options' => array(
-				'always' => lang('layout_field_frequency_always'),
-				'hourly' => lang('layout_field_frequency_hourly'),
-				'daily' => lang('layout_field_frequency_daily'),
-				'weekly' => lang('layout_field_frequency_weekly'),
-				'monthly' => lang('layout_field_frequency_monthly'),
-				'yearly' => lang('layout_field_frequency_yearly'),
-				'never' => lang('layout_field_frequency_never'),
+// initialize layout fields
+if (file_exists(APPPATH.'views/_layouts/none.php'))
+{
+	$config['layouts']['none'] = array(
+		'label' => 'None',
+		'fields' => array(
+			'copy' => array(
+				'type' => 'copy',
+				'label' => lang('layout_field_none_copy')
 			),
-			'value' => 'always',
-			'label' => lang('layout_field_frequency')
+			'body' => array(
+				'type' => 'textarea',
+				'label' => lang('layout_field_body')
+			)
 		)
-	)
-);
+	);
+}
 
-$config['layouts']['robots_txt'] = array(
-	'label' => 'robots.txt',
-	'fields' => array(
-		'copy' => array(
-			'type' => 'copy',
-			'label' => lang('layout_field_robots_txt_copy')
-		),
-		'body' => array(
-			'type' => 'textarea',
-			'label' => lang('layout_field_body'),
-			'value' => "User-agent: *\nDisallow: /fuel/", 'class' => 'no_editor'
+if (file_exists(APPPATH.'views/_layouts/301_redirect.php'))
+{
+	$config['layouts']['301_redirect'] = array(
+		'label' => '301 Redirect',
+		'fields' => array(
+			'copy' => array(
+				'type' => 'copy',
+				'label' => lang('layout_field_301_redirect_copy')
+			),
+			'redirect_to' => array('label' => lang('layout_field_redirect_to'))
 		)
-	)
-);
+	);
+}
 
-$config['layouts']['none'] = array(
-	'label' => 'None',
-	'fields' => array(
-		'copy' => array(
-			'type' => 'copy',
-			'label' => lang('layout_field_none_copy')
-		),
-		'body' => array(
-			'type' => 'textarea',
-			'label' => lang('layout_field_body')
+if (file_exists(APPPATH.'views/_layouts/alias.php'))
+{
+	$config['layouts']['alias'] = array(
+		'label' => 'Alias',
+		'fields' => array(
+			'copy' => array(
+				'type' => 'copy',
+				'label' => lang('layout_field_alias_copy')
+			),
+			'alias' => array('label' => lang('layout_field_alias')),
 		)
-	)
-);
+	);
+}
+
+if (file_exists(APPPATH.'views/_layouts/404_error.php'))
+{
+	$config['layouts']['404_error'] = array(
+		'label' => '404 Error',
+		'fields' => array(
+			'heading' => array('label' => lang('layout_field_heading')),
+			'body' => array('label' => lang('layout_field_body')),
+		)
+	);
+}
+
+if (file_exists(APPPATH.'views/_layouts/sitemap_xml.php'))
+{
+	$config['layouts']['sitemap_xml'] = array(
+		'label' => 'sitemap.xml',
+		'fields' => array(
+			'copy' => array(
+				'type' => 'copy',
+				'label' => lang('layout_field_sitemap_xml_copy')
+			),
+			'frequency' => array(
+				'type' => 'select',
+				'options' => array(
+					'always' => lang('layout_field_frequency_always'),
+					'hourly' => lang('layout_field_frequency_hourly'),
+					'daily' => lang('layout_field_frequency_daily'),
+					'weekly' => lang('layout_field_frequency_weekly'),
+					'monthly' => lang('layout_field_frequency_monthly'),
+					'yearly' => lang('layout_field_frequency_yearly'),
+					'never' => lang('layout_field_frequency_never'),
+				),
+				'value' => 'always',
+				'label' => lang('layout_field_frequency')
+			)
+		)
+	);
+}
+
+if (file_exists(APPPATH.'views/_layouts/robots_txt.php'))
+{
+	$config['layouts']['robots_txt'] = array(
+		'label' => 'robots.txt',
+		'fields' => array(
+			'copy' => array(
+				'type' => 'copy',
+				'label' => lang('layout_field_robots_txt_copy')
+			),
+			'body' => array(
+				'type' => 'textarea',
+				'label' => lang('layout_field_body'),
+				'value' => "User-agent: *\nDisallow: /fuel/", 'class' => 'no_editor'
+			)
+		)
+	);
+}
+
 
 /* End of file fuel_layouts.php */
 /* Location: ./modules/fuel/config/fuel_layouts.php */
