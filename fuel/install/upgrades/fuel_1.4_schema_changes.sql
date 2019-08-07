5d04e7fede23e13af772e4e7d3f69625782ef669
David McReynolds <dave@thedaylightstudio.com>
2019-08-07 08:05:01 -0700

    fix: for adding unique fuel_relationships index in 1.4 upgrade

--- fuel/install/upgrades/fuel_1.4_schema_changes.sql
+++ fuel/install/upgrades/fuel_1.4_schema_changes.sql
@@ -1,4 +1,6 @@
 ALTER TABLE `fuel_categories` CHANGE `slug` `slug` VARCHAR(255)  CHARACTER SET utf8  COLLATE utf8_general_ci  NOT NULL  DEFAULT '';
 ALTER TABLE `fuel_categories` CHANGE `name` `name` VARCHAR(255)  CHARACTER SET utf8  COLLATE utf8_general_ci  NOT NULL  DEFAULT '';
 
+# TO FIX POTENTIAL DUPLICATES CAUSED BY NOT HAVING THE UNIQUE INDEX
+DELETE n1 FROM fuel_relationships n1, fuel_relationships n2 WHERE n1.id > n2.id AND n1.candidate_table = n2.candidate_table AND n1.candidate_key = n2.candidate_key AND n1.foreign_table = n2.foreign_table AND n1.foreign_key = n2.foreign_key;
 ALTER TABLE `fuel_relationships` ADD UNIQUE INDEX (`candidate_table`, `candidate_key`, `foreign_table`, `foreign_key`);
