6b3147579dfdfd00fb715359029953ba3cf7ff8f
David McReynolds <dave@thedaylightstudio.com>
2020-12-15 14:09:25 -0800

    fix: for purifier to allow target=“_blank”

--- fuel/application/config/purifier.php
+++ fuel/application/config/purifier.php
@@ -1,18 +1,18 @@
 <?php 
 $config['settings'] = array(
 	'default' => array(
-//		'HTML.Trusted'             => TRUE, // For Javascript
-//		'HTML.SafeIframe'          => TRUE, // For iframes
+		//'HTML.Trusted'             => TRUE, // For Javascript... must also add 'script' to HTML.Allowed
+		//'HTML.SafeIframe'          => TRUE, // For iframes
 		'Attr.EnableID'            => TRUE,
-		'HTML.Doctype'             => 'XHTML 1.0 Strict',
-		'HTML.Allowed'             => 'div[id],b,strong,i,em,a[href|title],ul,ol,li,p[style],br,span[style],img[width|height|alt|src]',
+		'Attr.AllowedFrameTargets' => array('_blank'),
+		'HTML.Allowed'             => 'div[id],b,strong,i,em,a[href|title|target],ul,ol,li,p[style],br,span[style],img[width|height|alt|src]',
 		//'CSS.AllowedProperties'    => 'font,font-size,font-weight,font-style,font-family,text-decoration,padding-left,color,background-color,text-align,float,margin',
 		'AutoFormat.AutoParagraph' => FALSE, // This will cause errors if you globally apply this to input being saved to the database so we set it to false.
 		'AutoFormat.RemoveEmpty'   => TRUE,
 	),
 	'comment' => array(
 		'HTML.Doctype'             => 'XHTML 1.0 Strict',
-		'HTML.Allowed'             => 'p,a[href|title],abbr[title],acronym[title],b,strong,blockquote[cite],code,em,i,strike',
+		'HTML.Allowed'             => 'p,a[href|title|target],abbr[title],acronym[title],b,strong,blockquote[cite],code,em,i,strike',
 		'CSS.AllowedProperties'    => 'font,font-size,font-weight,font-style,font-family,text-decoration,padding-left,color,background-color,text-align,float,margin',
 		'AutoFormat.AutoParagraph' => TRUE, 
 		'AutoFormat.Linkify'       => TRUE,
