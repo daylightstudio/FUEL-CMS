6b0ead0b2939a3545ea9b216378e50562a4d63ce
David McReynolds <dave@thedaylightstudio.com>
2020-12-15 12:08:39 -0800

    fix: rewind… script and target aren’t allowed by purifier by default

--- fuel/application/config/purifier.php
+++ fuel/application/config/purifier.php
@@ -5,14 +5,14 @@ $config['settings'] = array(
 //		'HTML.SafeIframe'          => TRUE, // For iframes
 		'Attr.EnableID'            => TRUE,
 		'HTML.Doctype'             => 'XHTML 1.0 Strict',
-		'HTML.Allowed'             => 'script,div[id],b,strong,i,em,a[href|title|target],ul,ol,li,p[style],br,span[style],img[width|height|alt|src]',
+		'HTML.Allowed'             => 'div[id],b,strong,i,em,a[href|title],ul,ol,li,p[style],br,span[style],img[width|height|alt|src]',
 		//'CSS.AllowedProperties'    => 'font,font-size,font-weight,font-style,font-family,text-decoration,padding-left,color,background-color,text-align,float,margin',
 		'AutoFormat.AutoParagraph' => FALSE, // This will cause errors if you globally apply this to input being saved to the database so we set it to false.
 		'AutoFormat.RemoveEmpty'   => TRUE,
 	),
 	'comment' => array(
 		'HTML.Doctype'             => 'XHTML 1.0 Strict',
-		'HTML.Allowed'             => 'p,a[href|title],abbr[title|target],acronym[title],b,strong,blockquote[cite],code,em,i,strike',
+		'HTML.Allowed'             => 'p,a[href|title],abbr[title],acronym[title],b,strong,blockquote[cite],code,em,i,strike',
 		'CSS.AllowedProperties'    => 'font,font-size,font-weight,font-style,font-family,text-decoration,padding-left,color,background-color,text-align,float,margin',
 		'AutoFormat.AutoParagraph' => TRUE, 
 		'AutoFormat.Linkify'       => TRUE,
