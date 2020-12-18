78e58a60e819c9ed8fb33452f76a4224da8421eb
David McReynolds <dave@thedaylightstudio.com>
2020-12-18 08:03:39 -0800

    fix: added looser HTML.Doctype for purifier and examples for Iframes

--- fuel/application/config/purifier.php
+++ fuel/application/config/purifier.php
@@ -3,12 +3,14 @@ $config['settings'] = array(
 	'default' => array(
 		//'HTML.Trusted'             => TRUE, // For Javascript... must also add 'script' to HTML.Allowed
 		//'HTML.SafeIframe'          => TRUE, // For iframes
+		//'URI.SafeIframeRegexp'     => '%^(http://|https://|//)(www.youtube.com/embed/|player.vimeo.com/video/)%',
 		'Attr.EnableID'            => TRUE,
 		'Attr.AllowedFrameTargets' => array('_blank'),
 		'HTML.Allowed'             => 'div[id],b,strong,i,em,a[href|title|target],ul,ol,li,p[style],br,span[style],img[width|height|alt|src]',
 		//'CSS.AllowedProperties'    => 'font,font-size,font-weight,font-style,font-family,text-decoration,padding-left,color,background-color,text-align,float,margin',
 		'AutoFormat.AutoParagraph' => FALSE, // This will cause errors if you globally apply this to input being saved to the database so we set it to false.
 		'AutoFormat.RemoveEmpty'   => TRUE,
+		'HTML.Doctype'             => 'HTML 4.01 Transitional',
 	),
 	'comment' => array(
 		'HTML.Doctype'             => 'XHTML 1.0 Strict',
@@ -20,6 +22,6 @@ $config['settings'] = array(
 	),
 	'youtube' => array(
 		'HTML.SafeIframe'          => TRUE,
-		'URI.SafeIframeRegexp'     => "%^(http://|https://|//)(www.youtube.com/embed/|player.vimeo.com/video/)%",
+		'URI.SafeIframeRegexp'     => '%^(http://|https://|//)(www.youtube.com/embed/|player.vimeo.com/video/)%',
 	)
 );
